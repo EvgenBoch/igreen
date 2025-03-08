2763a6a53509168cc731356dd60a7d7caadaca93
EvgenBoch <daxterweb77@gmail.com>
2025-03-08 12:38:24 +0300

    Revert "add slider"
    This reverts commit 34b52dabedd79c42801663e76b3c35f971fb05fd.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    // Header slider
+    $('.presentation__slider').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.presentation__arrow-left'),
+        nextArrow: $('.presentation__arrow-right'),
+        //autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
+})
